5366b3dbc8f196b93eacea78f628b6267d5da0a3
Maikol2005 <miguel.garcia@itc-consulting.mx>
2025-02-05 13:45:55 -0600

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,28 @@
 self.addEventListener("push", (event) => {
-    const data = event.data.json();
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: "/calendario.ico", 
-    });
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: "/calendario.ico",
   });
-  
+});
+
+self.addEventListener("notificationclick", function (event) {
+  event.notification.close(); // Cierra la notificación al hacer clic
+
+  event.waitUntil(
+    clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((windowClients) => {
+        // Verifica si ya hay una ventana abierta con la app
+        for (let client of windowClients) {
+          if (client.url.includes("/eventos") && "focus" in client) {
+            return client.focus(); // Si ya está abierta, enfoca la pestaña
+          }
+        }
+        // Si no está abierta, abre una nueva ventana
+        if (clients.openWindow) {
+          return clients.openWindow("https://calendario-eventos-oropeza.netlify.app/");
+        }
+      })
+  );
+});
